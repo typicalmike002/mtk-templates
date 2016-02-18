9552d1df9f7e858bf14e07690ae8299d29341305
typicamike002 <townsendm105@gmail.com>
2016-02-18 00:55:14 -0500

    styled home template

--- index.php
+++ index.php
@@ -8,14 +8,14 @@ $page = new Page( $_GET['page'] );
 <html>
 <head>
 	<title>MTK Tavern | <?php echo $page->title; ?></title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
-	<div id="wrap">
-		<?php include('templates/header.html'); ?>
+	<?php include('templates/header.html'); ?>
+	<div id="main">
 		<?php include('templates/' . $page->request . '.html'); ?>
-		<?php include('templates/footer.html'); ?>
 	</div>
+	<?php include('templates/footer.html'); ?>
 </body>
 </html
